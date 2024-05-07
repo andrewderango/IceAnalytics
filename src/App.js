afcd0c96e4ead949d332205e984510cf50b971f5
Andrew De Rango <drewderango@gmail.com>
2024-05-07 01:18:54 -0400

    Add headers and footers for nav

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import Home from './pages/Home';
 import Players from './pages/Players';
 import Teams from './pages/Teams';
 import About from './pages/About';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
+      <Header />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/players" component={Players} />
         <Route path="/teams" component={Teams} />
         <Route path="/about" component={About} />
       </Switch>
+      <Footer />
     </Router>
   );
 }
