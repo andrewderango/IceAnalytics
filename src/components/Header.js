8177f963857b440bb5db61b985ebd870c2dc8afa
Andrew De Rango <drewderango@gmail.com>
2024-05-08 23:21:40 -0400

    Add header base UI

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Header.scss';
+import logo from '../assets/logo.svg';
 
 function Header() {
   return (
-    <header style={{ 
-      display: 'flex', 
-      justifyContent: 'space-between', 
-      padding: '10px', 
-      position: 'fixed', 
-      top: '0', 
-      width: '100%', 
-      background: '#333',
-      zIndex: '1000'
-    }}>
-      <h1 style={{ margin: '0' }}>PuckProjections</h1>
+    <header className="header">
+      <div className="header-name">
+        <img src={logo} alt="Logo" />
+        <h1>PUCKPROJECTIONS</h1>
+      </div>
       <nav>
-        <ul style={{ display: 'flex', gap: '10px', listStyle: 'none', margin: '0' }}>
-          <li><Link to="/home">Home</Link></li>
-          <li><Link to="/games">Games</Link></li>
-          <li><Link to="/players">Players</Link></li>
-          <li><Link to="/teams">Teams</Link></li>
-          <li><Link to="/about">About</Link></li>
+        <ul>
+          <li><Link to="/home">HOME</Link></li>
+          <li><Link to="/games">GAMES</Link></li>
+          <li><Link to="/players">PLAYERS</Link></li>
+          <li><Link to="/teams">TEAMS</Link></li>
+          <li><Link to="/about">ABOUT</Link></li>
         </ul>
       </nav>
     </header>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import '../styles/Home.scss';
 
 function Home() {
   return (
-    <div>
+    <div className="home">
       <h1>Welcome to PuckProjections</h1>
       <p>Explore NHL statistics and projections here.</p>
     </div>
