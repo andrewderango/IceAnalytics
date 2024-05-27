0a61b268e6714832629f26611176ca1b9c8bb398
Andrew De Rango <drewderango@gmail.com>
2024-05-27 01:03:16 -0400

    Add homepage top section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import '../styles/Home.scss';
+import headshot from '../assets/images/headshot6.png';
 
 function Home() {
   return (
     <div className="home">
-      <h1>Welcome to PuckProjections</h1>
-      <p>Explore NHL statistics and projections here.</p>
+      <div className="text-container">
+        <h1>PUCKPROJECTIONS</h1>
+        <div className="underline"></div>
+        <p>Transformative NHL projections and analytics powered by machine learning</p>
+      </div>
+      <img src={headshot} alt="Headshot" className="headshot" />
     </div>
   );
 }
