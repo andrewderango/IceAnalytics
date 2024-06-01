e26ab0989577ddd4d1739ed4548cd51726de6433
Andrew De Rango <drewderango@gmail.com>
2024-06-01 17:25:03 -0400

    Add about section to landing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,13 +21,24 @@ function Home() {
 
   return (
     <div className="home">
-      <div className="text-container slide-in-left">
-        <h1>PUCKPROJECTIONS</h1>
-        <div className="underline"></div>
-        <p>Transformative NHL projections and analytics powered by machine learning</p>
+      <div className="landing-section">
+        <div className="text-container slide-in-left">
+          <h1 className="slide-in-h1">PUCKPROJECTIONS</h1>
+          <div className="underline"></div>
+          <p>Transformative NHL projections and analytics powered by machine learning</p>
+        </div>
+        <img src={headshot} alt="Headshot" className="headshot slide-in-right" />
+        {!isScrolled && <div id="scroll-down-arrow"></div>}
       </div>
-      <img src={headshot} alt="Headshot" className="headshot slide-in-right" />
-      {!isScrolled && <div id="scroll-down-arrow"></div>}
+      <div className="about-section">
+        <h2>ABOUT PUCKPROJECTIONS</h2>
+        <div className="underline"></div>
+        <p>PuckProjections is a free and open-source NHL simulation engine used to deliver cutting-edge projections and analytics. The platform harnesses the power of ensemble machine learning and Monte Carlo simulations to provide comprehensive insights into NHL games, players, and teams.</p>
+        <button onClick={() => window.location.href='/about'}>
+          <span>LEARN MORE</span>
+        </button>
+        {/* <button onClick={() => window.open('https://github.com/andrewderango/NHL-Simulation-Engine', '_blank')}>View Source Code</button> */}
+      </div>
       <div className="scroll-down-text">Scroll down</div> {/* temp */}
     </div>
   );
