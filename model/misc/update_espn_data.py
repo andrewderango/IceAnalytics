9a92b7223c2fa9f8e61cff91b812a5a552ef9663
Andrew De Rango <drewderango@gmail.com>
2025-01-03 14:49:15 -0500

    update espn data func

--- model/misc/update_espn_data.py
+++ model/misc/update_espn_data.py
@@ -53,32 +53,55 @@ def replace_names(name):
         name = str(name)
     
     name_replacement_dict = {
-        'Jr.': 'Jr',
+        'è': 'e',
+        'é': 'e',
+        'ü': 'u',
+        'ö': 'o',
+        'ä': 'a',
+        'mathew': 'matt',
+        'alexander': 'alex',
+        'patrick': 'pat',
+        'janis': 'jj',
+        'johnny': 'john',
+        'nate': 'nathan',
     }
 
     for original, replacement in name_replacement_dict.items():
         name = name.replace(original, replacement)
     return name
 
-def update_player_bios(espn_df):
+def update_player_bios(espn_df, download_files, verbose):
     current_dir = os.path.dirname(os.path.abspath(__file__))
     fetch_path = os.path.join(current_dir, '..', '..', 'engine_data', 'Player Bios', 'Skaters', 'skater_bios.csv')
     skater_bios_df = pd.read_csv(fetch_path)
     skater_bios_df = skater_bios_df.drop(skater_bios_df.columns[0], axis=1)
 
+    # strip and lowercase names for comparison
     skater_bios_df['playerStrippedEspn'] = skater_bios_df['Player'].str.replace(' ', '').str.replace('.', '').str.replace('-', '').str.replace('\'', '').str.lower().apply(replace_names)
     espn_df['playerStrippedEspn'] = espn_df['Player'].str.replace(' ', '').str.replace('.', '').str.replace('-', '').str.replace('\'', '').str.lower().apply(replace_names)
 
-    # merge
-    merged_df = pd.merge(skater_bios_df, espn_df, how='outer', on='playerStrippedEspn')
-    print(merged_df)
-    save_path = os.path.join(current_dir, '..', '..', 'engine_data', 'Player Bios', 'Skaters', 'test.csv')
-    merged_df.to_csv(save_path, index=True)
-    quit()
+    # merge to update headshots for existing players
+    merged_df = pd.merge(skater_bios_df, espn_df[['playerStrippedEspn', 'EspnHeadshot']], how='outer', on='playerStrippedEspn')
+    if verbose:
+        print('Updated Player Bios:')
+        print(merged_df)
+    if download_files:
+        save_path = os.path.join(current_dir, '..', '..', 'engine_data', 'Player Bios', 'Skaters', 'skater_bios.csv')
+        merged_df.to_csv(save_path, index=True)
+
+    # check for failed merges
+    failed_df = pd.merge(skater_bios_df, espn_df[['playerStrippedEspn', 'EspnHeadshot']], how='right', on='playerStrippedEspn')
+    failed_df = failed_df[failed_df['PlayerID'].isnull()]
+    if verbose:
+        print('Failed Merges:')
+        print(failed_df)
+    if download_files:
+        save_path = os.path.join(current_dir, '..', '..', 'engine_data', 'Player Bios', 'Skaters', 'espn_failed_merge.csv')
+        failed_df.to_csv(save_path, index=True)
 
 def main():
     espn_df = pull_espn_data(limit=1000, download_files=True, verbose=True)
-    update_player_bios(espn_df)
+    update_player_bios(espn_df=espn_df, download_files=True, verbose=True)
 
 if __name__ == '__main__':
     main()
