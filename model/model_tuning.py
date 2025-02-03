ec599a8648ddfe46fe7a48cf92f9259d90101c47
Andrew De Rango <drewderango@gmail.com>
2025-02-03 02:39:02 -0500

    add and test more model types

--- model/model_tuning.py
+++ model/model_tuning.py
@@ -1,6 +1,9 @@
 import os
 import pandas as pd
 import xgboost as xgb
+from sklearn.svm import SVR
+from sklearn.linear_model import Ridge, Lasso, ElasticNet
+from sklearn.neural_network import MLPRegressor
 from sklearn.model_selection import KFold
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 
@@ -73,7 +76,7 @@ def aggregate_skater_offence_training_data(projection_year):
     # Data cleaning
     # combined_data = combined_data.loc[(combined_data['Y-3 GP'] >= 30) & (combined_data['Y-2 GP'] >= 30) & (combined_data['Y-1 GP'] >= 30) & (combined_data['Y-0 GP'] >= 30)]
     combined_data = combined_data.loc[(combined_data['Y-1 GP'] >= 30) & (combined_data['Y-0 GP'] >= 30)]
-    combined_data = combined_data[combined_data['Y-0'] != projection_year]
+    # combined_data = combined_data[combined_data['Y-0'] != projection_year]
     # combined_data.sort_values(by='Y-0 ATOI', ascending=False, inplace=True)
     combined_data.sort_values(by=['Player', 'Y-0'], ascending=[True, False], inplace=True)
     combined_data = combined_data.reset_index(drop=True)
@@ -82,7 +85,7 @@ def aggregate_skater_offence_training_data(projection_year):
 
     return combined_data
 
-def tune_goal_model(projection_year, verbose):
+def tune_goal_model(projection_year, model, verbose):
 
     train_data = aggregate_skater_offence_training_data(projection_year)
     train_data = train_data.loc[(train_data['Y-3 GP'] >= 30) & (train_data['Y-2 GP'] >= 30) & (train_data['Y-1 GP'] >= 30) & (train_data['Y-0 GP'] >= 30)]
@@ -92,85 +95,86 @@ def tune_goal_model(projection_year, verbose):
 
     # Define the feature columns
     train_data['Position'] = train_data['Position'].apply(lambda x: 0 if x == 'D' else 1)
-    feature_cols = ['Y-0', 'Y-3 Gper1kChunk', 'Y-2 Gper1kChunk', 'Y-1 Gper1kChunk', 'Y-3 xGper1kChunk', 'Y-2 xGper1kChunk', 'Y-1 xGper1kChunk', 'Y-3 SHper1kChunk', 'Y-2 SHper1kChunk', 'Y-1 SHper1kChunk', 'Y-3 iCFper1kChunk', 'Y-2 iCFper1kChunk', 'Y-1 iCFper1kChunk', 'Y-3 RAper1kChunk', 'Y-2 RAper1kChunk', 'Y-1 RAper1kChunk', 'Y-0 Age', 'Position']
+    feature_cols = ['Y-3 Gper1kChunk', 'Y-2 Gper1kChunk', 'Y-1 Gper1kChunk', 'Y-3 xGper1kChunk', 'Y-2 xGper1kChunk', 'Y-1 xGper1kChunk', 'Y-3 SHper1kChunk', 'Y-2 SHper1kChunk', 'Y-1 SHper1kChunk', 'Y-3 iCFper1kChunk', 'Y-2 iCFper1kChunk', 'Y-1 iCFper1kChunk', 'Y-3 RAper1kChunk', 'Y-2 RAper1kChunk', 'Y-1 RAper1kChunk', 'Y-0 Age', 'Position']
+
+    if model == 'xgboost':
+        feature_cols = ['Y-3 Gper1kChunk', 'Y-2 Gper1kChunk', 'Y-1 Gper1kChunk', 'Y-3 xGper1kChunk', 'Y-2 xGper1kChunk', 'Y-1 xGper1kChunk', 'Y-0 Age', 'Position']
+        n_estimators = 50
+        learning_rate = 0.1
+        max_depth = 3
+        min_child_weight = 1
+        subsample = 1.0
+        colsample_bytree = 1.0
+        model = xgb.XGBRegressor(
+            objective='reg:squarederror',
+            n_estimators=n_estimators,
+            learning_rate=learning_rate,
+            max_depth=max_depth,
+            min_child_weight=min_child_weight,
+            subsample=subsample,
+            colsample_bytree=colsample_bytree,
+            verbosity=0
+        )
+    
+    elif model == 'ridge':
+        # feature_cols = ['Y-3 Gper1kChunk', 'Y-2 Gper1kChunk', 'Y-1 Gper1kChunk', 'Y-3 xGper1kChunk', 'Y-2 xGper1kChunk', 'Y-1 xGper1kChunk', 'Y-0 Age', 'Position']
+        feature_cols = ['Y-3 Gper1kChunk', 'Y-2 Gper1kChunk', 'Y-1 Gper1kChunk', 'Y-3 xGper1kChunk', 'Y-2 xGper1kChunk', 'Y-1 xGper1kChunk', 'Y-3 SHper1kChunk', 'Y-2 SHper1kChunk', 'Y-1 SHper1kChunk', 'Y-3 iCFper1kChunk', 'Y-2 iCFper1kChunk', 'Y-1 iCFper1kChunk', 'Y-3 RAper1kChunk', 'Y-2 RAper1kChunk', 'Y-1 RAper1kChunk', 'Y-0 Age', 'Position']
+        model = Ridge(alpha=0.001)
+
+    elif model == 'lasso':
+        feature_cols = ['Y-3 Gper1kChunk', 'Y-2 Gper1kChunk', 'Y-1 Gper1kChunk', 'Y-3 xGper1kChunk', 'Y-2 xGper1kChunk', 'Y-1 xGper1kChunk', 'Y-3 SHper1kChunk', 'Y-2 SHper1kChunk', 'Y-1 SHper1kChunk', 'Y-3 iCFper1kChunk', 'Y-2 iCFper1kChunk', 'Y-1 iCFper1kChunk', 'Y-3 RAper1kChunk', 'Y-2 RAper1kChunk', 'Y-1 RAper1kChunk', 'Y-0 Age', 'Position']
+        model = Lasso(alpha=0.001)
+
+    elif model == 'neural_net':
+        feature_cols = ['Y-3 Gper1kChunk', 'Y-2 Gper1kChunk', 'Y-1 Gper1kChunk', 'Y-3 xGper1kChunk', 'Y-2 xGper1kChunk', 'Y-1 xGper1kChunk', 'Y-0 Age', 'Position']
+        model = MLPRegressor(
+            hidden_layer_sizes=(50, 30, 10),
+            max_iter=2000,
+            early_stopping=True,
+            validation_fraction=0.05,
+            learning_rate='adaptive',
+            learning_rate_init=0.005,
+            alpha=0.00001,
+            solver='adam',
+            activation='relu', 
+            batch_size=64,
+            random_state=42
+        )
+
+    elif model == 'support_vector':
+        feature_cols = ['Y-3 Gper1kChunk', 'Y-2 Gper1kChunk', 'Y-1 Gper1kChunk', 'Y-3 xGper1kChunk', 'Y-2 xGper1kChunk', 'Y-1 xGper1kChunk', 'Y-0 Age', 'Position']
+        model = SVR(
+            kernel='rbf', 
+            C=1.0, 
+            epsilon=0.80,
+            gamma='scale'
+        )
+
+    # Drop rows with missing values of features
     train_data = train_data.dropna(subset=feature_cols)
 
     # Separate the features and the target
     X = train_data[feature_cols]
-    y = train_data[['Y-0', 'Y-0 Gper1kChunk']]
+    y = train_data['Y-0 Gper1kChunk']
 
-    # Split the data into training and testing sets
-    X_train = X.loc[X['Y-0'] != 2024]
-    y_train = y.loc[y['Y-0'] != 2024]
-    X_test = X.loc[X['Y-0'] == 2024]
-    y_test = y.loc[y['Y-0'] == 2024]
+    # Perform 10-fold cross-validation
+    kf = KFold(n_splits=10, shuffle=True, random_state=42)
+    mse_scores = []
+    mae_scores = []
 
-    # remove y-0
-    X_train = X_train.drop(columns=['Y-0'])
-    X_test = X_test.drop(columns=['Y-0'])
-    y_train = y_train.drop(columns=['Y-0'])
-    y_test = y_test.drop(columns=['Y-0'])
+    for train_index, test_index in kf.split(X):
+        X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
 
-    # Define the model
-    n_estimators = 50
-    learning_rate = 0.1
-    max_depth = 3
-    min_child_weight = 1
-    subsample = 1.0
-    colsample_bytree = 1.0
-    model = xgb.XGBRegressor(
-        objective='reg:squarederror',
-        n_estimators=n_estimators,
-        learning_rate=learning_rate,
-        max_depth=max_depth,
-        min_child_weight=min_child_weight,
-        subsample=subsample,
-        colsample_bytree=colsample_bytree,
-        verbosity=0
-    )
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
 
-    # # Perform 10-fold cross-validation
-    # kf = KFold(n_splits=10, shuffle=True, random_state=42)
-    # mse_scores = []
-    # mae_scores = []
-
-    # for train_index, test_index in kf.split(X):
-    #     X_train, X_test = X.iloc[train_index], X.iloc[test_index]
-    #     y_train, y_test = y.iloc[train_index], y.iloc[test_index]
-
-    #     model.fit(X_train, y_train)
-    #     y_pred = model.predict(X_test)
-
-    #     mse_scores.append(mean_squared_error(y_test, y_pred))
-    #     mae_scores.append(mean_absolute_error(y_test, y_pred))
-
-
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
-
-    mse_scores = mean_squared_error(y_test, y_pred)
-    mae_scores = mean_absolute_error(y_test, y_pred)
+        mse_scores.append(mean_squared_error(y_test, y_pred))
+        mae_scores.append(mean_absolute_error(y_test, y_pred))
 
     if verbose:
-        # print(f'MSE Scores: {mse_scores}')
-        # print(f'MAE Scores: {mae_scores}')
-        # print(f'Mean MSE: {sum(mse_scores) / len(mse_scores)}')
-        # print(f'Mean MAE: {sum(mae_scores) / len(mae_scores) / 1000*2*60}')
-        print(f'MSE Score: {mse_scores}')
-        print(f'MAE Score: {mae_scores / 1000*2*60}')
-
-    # create a df for 2024 predictions
-    y_test['Y-0 Gper1kChunk Prediction'] = y_pred
-    y_test['PlayerID'] = train_data.loc[train_data['Y-0'] == 2024, 'PlayerID'].values
-    y_test['Player'] = train_data.loc[train_data['Y-0'] == 2024, 'Player'].values
-    y_test['Position'] = train_data.loc[train_data['Y-0'] == 2024, 'Position'].values
-    y_test['Y-0 Age'] = train_data.loc[train_data['Y-0'] == 2024, 'Y-0 Age'].values
-    y_test['G/60 Pred'] = y_test['Y-0 Gper1kChunk Prediction'] / 1000 * 2 * 60
-    y_test['G/60 Actual'] = y_test['Y-0 Gper1kChunk'] / 1000 * 2 * 60
-    y_test = y_test.drop(columns=['Y-0 Gper1kChunk', 'Y-0 Gper1kChunk Prediction'])
-    y_test = y_test.sort_values(by='G/60 Pred', ascending=False)
-    print(y_test)
-    quit()
+        print(f'Mean G/60 MSE: {sum(mse_scores) / len(mse_scores) * 9/625}')
+        print(f'Mean G/60 MAE: {sum(mae_scores) / len(mae_scores) * 3/25}')
 
     # Train the model on the full dataset
     model.fit(X, y)
@@ -178,4 +182,4 @@ def tune_goal_model(projection_year, verbose):
     return model
 
 PROJECTION_YEAR = 2025
-goal_model = tune_goal_model(projection_year=PROJECTION_YEAR, verbose=True)
+goal_model = tune_goal_model(projection_year=PROJECTION_YEAR, model='neural_net2', verbose=True)
