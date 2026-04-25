b3b85854a75d98bc806b23fa16baca7c06083fd7
Andrew De Rango <drewderango@gmail.com>
2026-04-24 23:08:21 -0400

    improve 404 screen

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,18 +3,14 @@ import { Link } from 'react-router-dom';
 import notFound from '../assets/images/404.png';
 import '../styles/NotFound.scss';
 import PageStatePanel from '../components/PageStatePanel';
-import { getUpcomingProjectionSeasonLabel } from '../utils/seasonLabels';
 
 function NotFound() {
-  const projectionSeason = getUpcomingProjectionSeasonLabel();
-
   return (
     <PageStatePanel
       wrapperClassName="not-found"
       title="404"
-      badge="Page Not Found"
       heading="This page does not exist"
-      message={`The page you requested could not be found. Use the links below to return to the main sections and follow ${projectionSeason} projections when they go live in July.`}
+      message={`The page you requested could not be found. Use the links below to return to the main sections.`}
       imageSrc={notFound}
       imageAlt="404"
       variant="not-found"
